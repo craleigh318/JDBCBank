e6d78412bdf44d446edde909eaa079185bc53859
Christopher Raleigh <craleigh318@users.noreply.github.com>
2019-01-19 00:46:59 -0500

    Updated factories.

--- src/main/java/com/revature/craleigh318/jdbc_bank/services/BankAccountFactory.java
+++ src/main/java/com/revature/craleigh318/jdbc_bank/services/BankAccountFactory.java
@@ -7,8 +7,8 @@ import com.revature.craleigh318.jdbc_bank.model.BankAccount;
 
 public class BankAccountFactory {
 	
-	private static final int BANK_ACCOUNT_ID = 0;
-	private static final int BANK_ACCOUNT_BALANCE = 1;
+	private static final int BANK_ACCOUNT_ID = 1;
+	private static final int BANK_ACCOUNT_BALANCE = 2;
 	
 	public BankAccount fromSQL(ResultSet resultSet) throws SQLException {
 		int id = resultSet.getInt(BANK_ACCOUNT_ID);
--- src/main/java/com/revature/craleigh318/jdbc_bank/services/UserFactory.java
+++ src/main/java/com/revature/craleigh318/jdbc_bank/services/UserFactory.java
@@ -1,5 +1,24 @@
 package com.revature.craleigh318.jdbc_bank.services;
 
-public class UserFactory {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import com.revature.craleigh318.jdbc_bank.model.User;
+
+public class UserFactory {
+	
+	private static final int USER_ID = 1;
+	private static final int BANK_ACCOUNT_ID = 2;
+	private static final int USERNAME = 3;
+	private static final int USER_PASSWORD = 4;
+	
+	public User fromSQL(ResultSet resultSet) throws SQLException {
+		int id = resultSet.getInt(USER_ID);
+		int bankAccountId = resultSet.getInt(BANK_ACCOUNT_ID);
+		String username = resultSet.getString(USERNAME);
+		String password = resultSet.getString(USER_PASSWORD);
+		return new User(id, bankAccountId, username, password);
+	}
+	
+	private UserFactory() { }
 }
