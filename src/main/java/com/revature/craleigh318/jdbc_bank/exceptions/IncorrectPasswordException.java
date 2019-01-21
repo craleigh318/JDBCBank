e300a736e77b06c9e72051ea6ccdf93b4b3abc50
Christopher Raleigh <craleigh318@users.noreply.github.com>
2019-01-21 16:37:23 -0500

    Update IncorrectPasswordException.java

--- src/main/java/com/revature/craleigh318/jdbc_bank/exceptions/IncorrectPasswordException.java
+++ src/main/java/com/revature/craleigh318/jdbc_bank/exceptions/IncorrectPasswordException.java
@@ -2,6 +2,6 @@ package com.revature.craleigh318.jdbc_bank.exceptions;
 
 public class IncorrectPasswordException extends Exception {
 	
-	public static final String MESSAGE = "Incorrect username or password.";
+	public static final String MESSAGE = "Incorrect password.";
 	private static final long serialVersionUID = 1724724677618907160L;
 }
